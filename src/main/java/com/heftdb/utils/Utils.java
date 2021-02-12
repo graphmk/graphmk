0437f7b229dced87fee5b288472819115b031a76
keshan <keshan011@live.com>
2021-02-12 14:38:27 +0530

    util classes

---
+++ src/main/java/com/heftdb/utils/Utils.java
@@ -0,0 +1,69 @@
+package com.heftdb.utils;
+
+import com.heftdb.mkcore.interfaces.Vertex;
+
+import java.io.*;
+import java.text.Normalizer;
+
+public class Utils {
+
+    public static void flushToFile(final File file, final String content) {
+        try {
+            if (file.exists()) {
+                file.delete();
+                file.createNewFile();
+            }
+            final FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+            final BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content + "\n");
+            bw.close();
+            fw.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String printGMLVertex(final Vertex v) {
+        final String delim = " ";
+        String props = "";
+        props = props + "id" + delim + toASCII(escStr(v.getId())) + delim;
+        for (final String o : v.getPropertyKeys()) {
+            props = props + delim + o.toString() + delim + toASCII(escStr(v.getProperty(o))) + delim;
+        }
+        final String toRet = " node [" + props + "]";
+        return toRet;
+    }
+
+    private static Object escStr(final Object s) {
+        if (s instanceof String) {
+            return "\"" + s + "\"";
+        }
+        return s;
+    }
+
+    public static String printGMLEdge(final Vertex source, final Vertex dest) {
+        final String delim = " ";
+        String props = "";
+        props = props + delim + "source" + delim + toASCII(escStr(source.getId())) + delim + "target" + delim + toASCII(escStr(dest.getId())) + delim;
+        final String toRet = " edge [" + props + "]";
+        return toRet;
+    }
+
+    public static Object toASCII(final Object s) {
+        if (s instanceof String) {
+            final String s2 = Normalizer.normalize((CharSequence)s, Normalizer.Form.NFKD);
+            final String regex = "[\\p{InCombiningDiacriticalMarks}\\p{IsLm}\\p{IsSk}]+";
+            String s3 = "encode_error";
+            try {
+                s3 = new String(s2.replaceAll(regex, "").getBytes("ascii"), "ascii");
+            }
+            catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return s3;
+        }
+        return s;
+    }
+
+}
---
+++ src/main/java/com/heftdb/utils/VirtualDegreeComparator.java
@@ -0,0 +1,27 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package com.heftdb.utils;
+
+import java.util.Map;
+import java.util.Comparator;
+
+public class VirtualDegreeComparator implements Comparator<String>
+{
+    boolean incr;
+    Map<String, Integer> virtualIndex;
+    
+    public VirtualDegreeComparator(final Map<String, Integer> vIndex, final boolean incr) {
+        this.incr = incr;
+        this.virtualIndex = vIndex;
+    }
+    
+    @Override
+    public int compare(final String o1, final String o2) {
+        if (this.incr) {
+            return this.virtualIndex.get(o1) - this.virtualIndex.get(o2);
+        }
+        return this.virtualIndex.get(o2) - this.virtualIndex.get(o1);
+    }
+}
